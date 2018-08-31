c6dcea757146788496610e6c2e03a833842fb79b
sebastianscheibe <root@techstack.de>
2018-08-31 10:41:23 +0200

    Add router helper for query params

--- src/backend/routes/RouterHelper.ts
+++ src/backend/routes/RouterHelper.ts
@@ -1,7 +1,17 @@
 export class RouterHelper {
-	static asyncMiddleware = fn =>
-		(req, res, next) => {
+
+	public static parseQueryParam(queryParam, defaultValue) {
+        if(queryParam) {
+            return parseInt(queryParam);
+        } else {
+            return defaultValue;
+        }
+	}
+
+	public static asyncMiddleware(fn){
+	    return (req, res, next) => {
 			Promise.resolve(fn(req, res, next))
 				.catch(next);
-		};
+		}
+    }
 }
--- src/backend/routes/TVMazeShowRouter.ts
+++ src/backend/routes/TVMazeShowRouter.ts
@@ -2,14 +2,14 @@ import {RouterHelper} from "./RouterHelper";
 import {TVMazeShowModel} from "../models/TVMazeShowModel";
 
 export class TVMazeShowRouter {
-    private static  ROUTER_PREFIX = 'tvshows';
+    private static ROUTER_PREFIX = 'tvshows';
 
     public static create(router) {
         router.get(`/${this.ROUTER_PREFIX}/`, RouterHelper.asyncMiddleware(async (req, res, next) => {
-            const limit = req.query.limit || 20;
-            const offset = req.query.offset || 0;
+            const limit = RouterHelper.parseQueryParam(req.query.limit, 20);
+            const offset = RouterHelper.parseQueryParam(req.query.offset, 0);
 
-            res.json(await TVMazeShowModel.find().sort({tvmazeid:1}).limit(limit).skip(offset));
+            res.json(await TVMazeShowModel.find().sort({tvmazeid: 1}).limit(limit).skip(offset));
         }));
     }
 }
